d92a04e5c223cb0f7e507747d9beb51f7e5916ba
happycube <Chad.Page@gmail.com>
2015-07-21 19:48:59 -0700

    added a few more comments

--- distance.c
+++ distance.c
@@ -46,37 +46,37 @@ int main(int argc, char **argv) {
   fscanf(f, "%lld", &words);
   fscanf(f, "%lld", &size);
 
+  // Allocate the vocabulary 2-dimensional array
   vocab = (char * *)malloc((long long)words * sizeof(char *));
   for (a = 0; a < words; a++) vocab[a] = (char *)malloc(max_w * sizeof(char));
-  
-  for (a = 0; a < N; a++) bestw[a] = -1;
 
-  //M = (float *)malloc((long long)words * (long long)size * sizeof(float));
+  // Allocate normalization values and vec arrays  
   norm = (float *)malloc((long long)words * sizeof(float));
   M = (float * *)malloc((long long)words * sizeof(float *));
-  // XXX: not checking for proper alloc, since this is temp code
+  // XXX: not checking for proper allocations, since this is temp code
   for (a = 0; a < words; a++) M[a] = (float *)malloc(size * sizeof(float));
 
+  // Load the words and vectors 
   for (b = 0; b < words; b++) {
     a = 0;
     while (1) {
       vocab[b][a] = fgetc(f);
-      if (feof(f) || (vocab[b][a] == ' ')) break;
-      if ((a < (max_w - 1)) && (vocab[b][a] != '\n')) a++;
+      if (feof(f) || (vocab[b][a] == ' ') || (a == max_w - 1)) break;
+      if (vocab[b][a] != '\n') a++;
     }
     vocab[b][a] = 0;
-    len = 0;
 
+    // Load the vector array
     fread(&M[b][0], sizeof(float), size, f);
-    for (a = 0; a < size; a++) {
-//        fread(&M[b][a], sizeof(float), 1, f);
+    len = 0;
+    for (; a < size; a++) {
         len += (M[b][a] * M[b][a]);
     }
-    norm[b] = sqrt(len);
-  //  printf("%d %s %f\n", b, vocab[b], len);
-//    for (a = 0; a < size; a++) M[b][a] /= len;
+    norm[b] = sqrt(len); // Save the normalization value
   }
   fclose(f);
+
+  // Main loop
   while (1) {
     printf("Enter word or sentence (EXIT to break): ");
     a = 0;
@@ -108,6 +108,8 @@ int main(int argc, char **argv) {
       }
     }
     cn++;
+
+    // Scan the dictionary for each input word
     for (a = 0; a < cn; a++) {
       for (b = 0; b < words; b++) if (!strcmp(vocab[b], st[a])) break;
       if (b == words) b = -1;
@@ -120,40 +122,55 @@ int main(int argc, char **argv) {
     }
     if (b == -1) continue;
     printf("\n                                              Word       Cosine distance\n------------------------------------------------------------------------\n");
+
+	// Add the vectors of each selected word
     for (a = 0; a < size; a++) vec[a] = 0;
     for (b = 0; b < cn; b++) {
       if (bi[b] == -1) continue;
       for (a = 0; a < size; a++) vec[a] += M[bi[b]][a];
     }
+
+	// Normalize the summed vector
     len = 0;
     for (a = 0; a < size; a++) len += vec[a] * vec[a];
     len = sqrt(len);
     for (a = 0; a < size; a++) vec[a] /= len;
-    for (a = 0; a < N; a++) {
-	bestd[a] = -1;
-	bestw[a] = -1;
-    }
-    for (c = 0; c < words; c++) {
-      a = 0;
-      for (b = 0; b < cn; b++) if (bi[b] == c) a = 1;
-      if (a == 1) continue;
-      dist = 0;
-      for (a = 0; a < size; a++) dist += vec[a] * M[c][a]; // / norm[c]);
-      dist /= norm[c];
-      if (dist > bestd[N - 1]) {
-	      for (a = 0; (a < N); a++) {
-		if (dist > bestd[a]) {
-			memmove(&bestd[a + 1], &bestd[a], sizeof(float) * (N - a - 1));
-			memmove(&bestw[a + 1], &bestw[a], sizeof(long long) * (N - a - 1));
 
-			bestd[a] = dist;
-			bestw[a] = c;
-			break;
-        	}
-        }
-      }
+	// Clear the list of best matches
+    for (a = 0; a < N; a++) {
+	bestd[a] = bestw[a] = -1;
     }
-    for (a = 0; a < N; a++) printf("%50s\t\t%f\n", vocab[bestw[a]], bestd[a]);
+
+	// Now do the actual comparison work
+	for (c = 0; c < words; c++) {
+		// Skip any words contained in our phrase
+		a = 0;
+		for (b = 0; b < cn; b++) if (bi[b] == c) a = 1;
+		if (a == 1) continue;
+
+		// Take the product of our phrase and the target word
+		dist = 0;
+		for (a = 0; a < size; a++) dist += vec[a] * M[c][a]; 
+		dist /= norm[c]; // Normalize
+
+		// If this is one of the best matches, put it in the list
+		if (dist > bestd[N - 1]) {
+			for (a = 0; (a < N); a++) {
+			if (dist > bestd[a]) {
+				memmove(&bestd[a + 1], &bestd[a], sizeof(float) * (N - a - 1));
+				memmove(&bestw[a + 1], &bestw[a], sizeof(long long) * (N - a - 1));
+
+				bestd[a] = dist;
+				bestw[a] = c;
+				break;
+			}
+		}
+	}
+    }
+    // All done!  Now print the list
+    for (a = 0; a < N; a++) {
+	if (bestw[a] >= 0) printf("%50s\t\t%f\n", vocab[bestw[a]], bestd[a]);
+    }
   }
   return 0;
 }
